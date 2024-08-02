02c47e985a11ba914e4ee18a7b75a25093b4e9c9
clintondotcpp <chukwunyereclinton@gmail.com>
2024-08-02 21:17:57 +0100

    third commit

--- linked_list.c
+++ linked_list.c
@@ -7,40 +7,72 @@ struct Node
     struct Node *next;
 };
 
-struct Node *head;
-
-void Insert(int data)
+struct Node *createNode(int data)
 {
-    struct Node *temp = (struct Node *)malloc(sizeof(struct Node *));
-    head = NULL;
-    temp->data = data;
-    temp->next = head;
-    head = temp;
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node *));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
 }
 
-void Print()
+void insertAtEnd(struct Node **head, int data)
 {
-    struct Node *temp = head;
-    printf("The List is: ");
-    while (temp != NULL)
+    struct Node *temp = *head;
+    temp->data = data;
+    temp->next = NULL;
+    if (*head = NULL)
     {
-        printf("%d", temp->data);
+        *head = temp;
+        return;
+    }
+    while (temp->next != NULL)
+    {
         temp = temp->next;
     }
-    printf("\n");
+    temp->next = *head;
+    *head = temp;
+}
+
+void insertNodeAtNthPos(struct Node **head, int data, int position)
+{
+    struct Node *temp = createNode(data);
+    temp->data = data;
+    temp->next = NULL;
+    if (position == 1)
+    {
+        temp->next = *head;
+        *head = temp;
+        return;
+    }
+
+    struct Node *temp2 = *head;
+    for (int i = 0; i < position - 2; i++)
+    {
+        temp2 = temp2->next;
+    }
+    temp->next = temp2->next;
+    temp2->next = temp;
+}
+
+void displayList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
 }
 
 int main()
 {
-    head = NULL;
-    int n, x;
-    printf("How many elements do you want in the array");
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
-    {
-        printf("Enter values for the array");
-        scanf("%d", &x);
-        Insert(x);
-        Print();
-    }
+    struct Node *head = NULL;
+    insertAtEnd(&head, 1);
+    insertNodeAtNthPos(&head, 522, 1);
+    insertNodeAtNthPos(&head, 111, 2);
+    insertNodeAtNthPos(&head, 13, 3);
+    insertNodeAtNthPos(&head, 22, 4);
+    insertNodeAtNthPos(&head, 15, 5);
+    displayList(head);
 }
--- linked_list_delete_key.c
+++ linked_list_delete_key.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Code to insert a node at the end of the linked list
-
 struct Node
 {
     int data;
@@ -11,7 +9,7 @@ struct Node
 
 struct Node *createNode(int data)
 {
-    struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *temp = (struct Node *)malloc(sizeof(struct Node *));
     temp->data = data;
     temp->next = NULL;
     return temp;
@@ -19,61 +17,68 @@ struct Node *createNode(int data)
 
 void insertNodeAtBeginning(struct Node **head, int data)
 {
-    struct Node *temp = createNode(data); // create node
-    temp->data = data;                    // set data part to data
-    temp->next = *head;                   // point the link part to head
-    *head = temp;                         // assign temp to head so
+    struct Node *temp = createNode(data);
+    temp->data = data;
+    temp->next = *head;
+    *head = temp;
 }
 
-void insertNodeAtEnd(struct Node **head, int data)
+void insertNodeNthPos(struct Node **head, int data, int position)
 {
-    struct Node *newNode = createNode(data);
-    if (*head == NULL)
+    struct Node *temp = createNode(data);
+    temp->data = data;
+    temp->next = NULL;
+    if (position == 1)
     {
-        *head = newNode;
+        *head = temp;
         return;
     }
-    struct Node *temp = *head; // temporary pointer to traverse the list
-    while (temp->next != NULL)
-    { // We traverse the list by making sure the next node exists
-        temp = temp->next;
+
+    struct Node *temp2 = *head;
+    for (int i = 0; i < position - 2; i++)
+    {
+        temp2 = temp2->next;
     }
-    temp->next = newNode; // We point the next part of temp to the newly created node
+
+    temp->next = temp2->next;
+    temp2->next = temp;
 }
 
-void deleteNode(struct Node **head, int key)
+void deleteNodeKey(struct Node **head, int key)
 {
-    struct Node *temp = *head;
-    struct Node *prev = NULL;
+    struct Node *temp = *head; // Temporary pointer to traverse the list
+    struct Node *prev = NULL;  // Pointer to keep track of the previous node
 
-    if (temp == NULL && temp->data == key)
-    {                       // If the head is the only node
-        *head = temp->next; // set head head to the new node
-        free(temp);         // free the deleted node
+    // Let's check if the head node holds the key to be deleted
+    if (temp != NULL && temp->data == key)
+    {
+        *head = temp->next; // change the head
+        free(temp);
         return;
     }
 
-    // do a search for the key
+    // Let's search for the key to be deleted
+
     while (temp != NULL && temp->data != key)
     {
-        prev = temp->next;
-        temp = *head;
+        prev = temp;
+        temp = temp->next;
     }
 
-    // If the key was not present in the list
+    // if the key was not present in the list
     if (temp == NULL)
         return;
 
-    prev->next = temp->next; // Unlink the node from the linked list
+    prev->next = temp->next;
     free(temp);
 }
 
-void displayList(struct Node *head)
+void Print(struct Node **head)
 {
-    struct Node *temp = head;
+    struct Node *temp = *head;
     while (temp != NULL)
     {
-        printf("%d -> ", temp->data);
+        printf("%d - > ", temp->data);
         temp = temp->next;
     }
     printf("NULL\n");
@@ -82,9 +87,10 @@ void displayList(struct Node *head)
 int main()
 {
     struct Node *head = NULL;
-    insertNodeAtBeginning(&head, 5);
-    insertNodeAtEnd(&head, 11);
-    insertNodeAtEnd(&head, 71);
-    insertNodeAtEnd(&head, 93);
-    displayList(head);
+    insertNodeAtBeginning(&head, 101);
+    insertNodeNthPos(&head, 21, 2);
+    insertNodeNthPos(&head, 13, 3);
+    insertNodeNthPos(&head, 349, 4);
+    insertNodeNthPos(&head, 213, 5);
+    Print(&head);
 }
